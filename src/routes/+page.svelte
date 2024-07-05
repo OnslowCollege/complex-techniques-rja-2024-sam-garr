cd83e5b20226e16bd35bbae10d1076544112b320
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-07-05 04:48:35 +0000

    Jul 5, 2024, 4:48 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,49 @@
 
 <script lang="ts">
     type State = 'start' | 'player turn' | 'opponent turn' | 'paused' | 'won' | 'lost'
+
+    let state: State = 'start'
+
+    function pauseGame(e: KeyboardEvent) {
+        if (e.key === 'Escape' ) {
+            switch (state) {
+                case 'playing': 
+                    state = 'paused'
+                    break
+                case 'paused':
+                    state = 'playing'
+                    break
+
+            }
+        }
+    }
+
+    function resetGame() {
+    }
+
+
+    /* When game is won give option to reset */
+    function gameWon() {
+        state = 'won'
+        resetGame()
+    }
+
+
+    /* When game is lost give option to reset */
+    function gameLost() {
+        state = 'lost'
+        resetGame()
+    }
+
 </script>
+
+<svelte:window on:keydown={pauseGame}/>
+
+{#if state === 'paused'}
+    <h1>Game paused</h1>
+{/if}
+
+{#if state === 'start'}
+    <h1>Matching game</h1>
+    <button on:click = {() => state = 'playing'}>Play</button>
+{/if}
