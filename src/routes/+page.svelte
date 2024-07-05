b259fcfccbc7d5d725d3db3b7785b46e3c92e177
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-07-05 04:37:07 +0000

    Jul 5, 2024, 4:37 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Last Card</h1>
+<p>Opponent cards</p>
+<p>Playing cards</p>
+<p>Players cards</p>
