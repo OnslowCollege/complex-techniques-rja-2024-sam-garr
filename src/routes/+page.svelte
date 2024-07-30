46e499a1a1b7ee34557afab5e27063064cec387d
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-07-30 02:49:42 +0000

    Jul 30, 2024, 2:49 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -202,6 +202,7 @@
 {/if}
 
 {#if state === 'opponentTurn'}
+    {opponentTurn()}
     <div class="cards">
         {#each oppositionCards as oppositionHandCard}
         <button on:click = {() => clicked = oppositionHandCard} on:click = {() => state = "playerTurn"} class="card">
